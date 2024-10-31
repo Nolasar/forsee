ba79c8b3115422a7b9fc3a0cb38f5b25feb9e4b9
Loassar <akhmarovruslant@gmail.com>
2024-11-01 00:57:45 +0300

    Simple MLP

---
+++ src/models/model.py
@@ -0,0 +1,91 @@
+from typing import List 
+from src.layers.dense import Dense
+
+class MLP:
+    def __init__(self, *args):
+        '''
+        Parameters
+        ----------
+        ***args**: List[Layer] or Layer
+            Takes instances of Layer class in input order
+        '''
+        self.layers: List[Dense] = [*args]
+        self.history = {
+            'loss': [],
+            'metric': []
+        }
+
+    def _build(self, X):
+        '''
+        Connect layers with each other and initialize weights
+        '''
+        prev_units = X.shape[1]
+        for layer in self.layers:
+            layer.build(prev_units)
+            prev_units = layer.units
+
+    def _feedforward(self, X):
+        '''
+        Implementation of forward propagation  
+
+        '''
+        prev_out = X
+        for layer in self.layers:
+            prev_out = layer.forward(prev_out)
+
+        return prev_out
+    
+    def _backprop(self, loss_grad, lr):
+        '''
+        Implementation of backward propagation  
+
+        '''
+        prev_grad = loss_grad
+        for layer in reversed(self.layers):
+            prev_grad = layer.backward(prev_grad, lr)
+
+    def train(self, X, y, loss_fn, metric_fn, epochs = 250, lr = 0.001) -> None:
+        '''
+        Main function for training model
+
+        Parameters
+        ----------
+        **X**: np.ndarray
+            feature matrix
+        **y**: np.ndarray
+            target vector
+        **loss_fn**: class \<loss\>
+            loss function (`MSE`, for example)
+        **metric_fn**: class \<metric\>
+            metric function (`Accuracy`, for example)
+        **epochs**: int, optional, default = 250
+            number of learning iterations
+        **lr**: float, optional, default = 0.001
+            learning rate
+
+        Returns
+        -------
+        None
+        '''
+        self._build(X) # initialize weights 
+
+        for epoch in range(epochs):
+
+            y_pred = self._feedforward(X) # calculate predictions 
+
+            loss = loss_fn(y_true=y, y_pred=y_pred) # compute loss
+
+            dloss = loss_fn.backward(y_true=y, y_pred=y_pred) # compute gradient of loss
+
+            self._backprop(dloss, lr) # compute grads and update weights and biases
+            
+            metric = metric_fn(y_true=y, y_pred=y_pred) # calculate metric
+            
+            self.history['loss'].append(loss) # save loss history 
+            self.history['metric'].append(metric) # save metric history
+
+            print(f'Epoch {epoch} | Loss {loss:.6f} | Metric {metric: .3f}')
+
+    
+    def predict(self, X):
+        return self._feedforward(X)
