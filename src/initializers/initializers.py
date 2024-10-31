216ac65ebe9f406a02feb2de219e4d6eb22d74b3
Loassar <akhmarovruslant@gmail.com>
2024-11-01 00:55:35 +0300

    GlorotUniform and Zeros initializers

---
+++ src/initializers/initializers.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class GlorotUniform:
+    def __call__(self, shape, random_state = 42):
+        rng = np.random.default_rng(random_state)
+        limit = np.sqrt(6 / (shape[0] + shape[1]))
+        return rng.uniform(low=-limit, high=limit, size=shape)   
+    
+class Zeros:
+    def __call__(self, shape):
+        return np.zeros(shape=shape)
+    
